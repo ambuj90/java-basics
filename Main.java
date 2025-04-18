6248d30f69544e37c8e0dbc9f49aed1c5189b16c
ambuj4 <ambuj@blueribbonmedia.com>
2025-04-19 00:00:24 +0530

    Initial Java program commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        // This is a comment
+        System.out.println("Ambuj Sharma");
+        /* The code below will print the words Hello World
+        to the screen, and it is amazing */
+        System.out.println("Java with Automation");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+    }
+}
---
+++ myOperator.java
@@ -0,0 +1,11 @@
+public class myOperator {
+    public static void main(String[] args) {
+        int sum1 = 20;
+        int sum2 = 30;
+        int sum = sum1 + sum2;
+        System.out.println(sum);
+        int x = 5;
+        int y = 3;
+        System.out.println(x > y);
+    }
+}
